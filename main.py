c1cd6e670616b62bc40c4c0cbbd166dd09000fbb
DevelopmentScientist420 <liam.schembri94@gmail.com>
2025-03-17 18:00:08 +0100

    Add vercel.json for deployment

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
+from pymongo import MongoClient
 
 app = FastAPI()
-
+uri = "mongodb+srv://liam:OoNpDaWeRtRhnJyU@mcastcluster.hwo2c.mongodb.net/?retryWrites=true&w=majority&appName=MCASTCluster"
 
 @app.get("/")
 async def root():
@@ -11,3 +12,11 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+client = MongoClient(uri)
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
